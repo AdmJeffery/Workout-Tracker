e247ab7220edde635f9548f5adae5a2b52ffa39d
Ammon Jeffery <savingphace@gmail.com>
2020-09-21 13:45:48 -0600

    created models folder

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("experess");
-const logger = require()
+const logger = require("morgan")
 const mongoose = require("mongoose");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
