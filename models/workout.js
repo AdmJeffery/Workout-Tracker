0e4b5e3f3e2974f46f27fdf7eb800b60f077834c
Ammon Jeffery <savingphace@gmail.com>
2020-09-22 19:04:02 -0600

    fixed an error in workout.js, continued schema.

--- models/workout.js
+++ models/workout.js
@@ -9,7 +9,29 @@ const exerciseSchema = new Schema(
             default: () => new Date()
         },
         exercises: [
-            {}
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter a valid exercise"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter the name of the exercise"
+                },
+                duration: {
+                    type: Number,
+                    required: "Please enter the duration"
+                },
+                weight: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                }
+            }
         ]
     }
 )
