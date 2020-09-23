34e9cafbdd935e05e6ea529f80078581f8609df8
Ammon Jeffery <savingphace@gmail.com>
2020-09-23 11:42:14 -0600

    created put and post routes.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post
+router.post("/api/workouts", (req, data) =>{
+    Workout.create({})
+        .then(dbWorkout =>{
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        }) 
+});
+
+router.put("/api/workouts/:id", ({body, params}, res) =>{
+    Workout.findByIdAndUpdate(
+        params.id, { $push: {exercises:body} }, { $new: ture, runValidators: true}
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
